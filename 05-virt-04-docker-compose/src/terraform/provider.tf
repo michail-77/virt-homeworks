075401b8c1087fa510a0121065918ef56a58cf75
Mikhail <michail.77@mail.ru>
2023-05-07 21:45:03 +0300

    11

--- 05-virt-04-docker-compose/src/terraform/provider.tf
+++ 05-virt-04-docker-compose/src/terraform/provider.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "yandex" {
-  token = "AQAAAAAIkgGxAATuwW6RRMSneE2uuV6vpMTK0Jw"
   service_account_key_file = "key.json"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
